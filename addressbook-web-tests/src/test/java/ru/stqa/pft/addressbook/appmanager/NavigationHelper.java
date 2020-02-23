79cc7b324cbba10f21675440c1060bdd52bd66d6
SidukovV <46403364+SiddVit@users.noreply.github.com>
2020-02-23 12:44:27 +0300

    Добавил определение положения.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -9,7 +9,12 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-       click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("New"))) {
+            return;
+        }
+        click(By.linkText("groups"));
     }
 
     public void returnToGroupPage() {
@@ -17,6 +22,9 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home page"));
     }
 
