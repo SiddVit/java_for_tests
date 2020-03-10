8272a2c1e67874c2f312bffcfbaac8fc85147226
SidukovV <46403364+SiddVit@users.noreply.github.com>
2020-03-10 12:02:26 +0300

    Revert "Добавил распознование"
    This reverts commit c89785c78cebb6bbda17e9fbe1cebae56e5363ce.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() {
